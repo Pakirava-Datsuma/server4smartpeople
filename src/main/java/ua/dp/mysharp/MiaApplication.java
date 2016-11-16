eef4b8cf005d60ea74ad3bdd904f8c0da42c0c10
Dmitriy Kuleshov <pakirava.datsuma@gmail.com>
2016-11-17 01:11:03 +0200

    initial commit

---
+++ src/main/java/ua/dp/mysharp/MiaApplication.java
@@ -0,0 +1,12 @@
+package ua.dp.mysharp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/dp/mysharp/MiaApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.dp.mysharp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
