0eb9c8495b4d36f8fd2bd6bb3adc1471673ee8a9
Dmitriy Kuleshov <pakirava.datsuma@gmail.com>
2016-12-08 03:28:48 +0200

    react-router rendering fix

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, Router, IndexRoute, hashHistory, browserHistory} from 'react-router';
-import {Main} from './component/Main';
-import {AdminPanel} from './component/AdminPanel';
-import {UserPanel} from './component/UserPanel';
-import {HousePanel} from './component/HousePanel';
+import {Route, Router, IndexRoute, browserHistory} from 'react-router';
+import Main from './component/Main';
+import AdminPanel from './component/AdminPanel';
+import UserPanel from './component/UserPanel';
+import HousePanel from './component/HousePanel';
 // import './index.css';
 
 console.log("injecting router to root...");
@@ -12,9 +12,9 @@ console.log("injecting router to root...");
 ReactDOM.render((
         <Router history={browserHistory}>
             <Route path="/" component={Main}>
-                {/*<IndexRoute component={AdminPanel}/>*/}
-                {/*<Route path="user/:id" component={UserPanel}/>*/}
-                {/*<Route path="house/:id" component={HousePanel}/>*/}
+                <IndexRoute component={AdminPanel}/>
+                <Route path="user/:id" component={UserPanel}/>
+                <Route path="house/:id" component={HousePanel}/>
             </Route>
         </Router>
     ),
